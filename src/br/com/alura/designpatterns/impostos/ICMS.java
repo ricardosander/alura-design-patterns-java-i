a4e483b3293394ec145a5388385db2aa90701e1d
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2016-12-12 09:44:35 -0200

    Ajuste no valor do imposto.

--- src/br/com/alura/designpatterns/impostos/ICMS.java
+++ src/br/com/alura/designpatterns/impostos/ICMS.java
@@ -3,6 +3,6 @@ package br.com.alura.designpatterns.impostos;
 public class ICMS implements Imposto {
 
 	public double calcula(Orcamento orcamento) {
-		return orcamento.getValor() * 0.1;
+		return orcamento.getValor() * 0.05 + 50;
 	}
 }
