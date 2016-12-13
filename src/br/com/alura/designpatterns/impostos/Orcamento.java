0914cd311c1c8cc267c4942cc37231475db1e839
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2016-12-13 13:51:46 -0200

    Modificações na classe orçamento.

--- src/br/com/alura/designpatterns/impostos/Orcamento.java
+++ src/br/com/alura/designpatterns/impostos/Orcamento.java
@@ -1,15 +1,29 @@
 package br.com.alura.designpatterns.impostos;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Orcamento {
 	
+	private final List<Item> itens;
 	private double valor;
 
-	public Orcamento(double valor) {
-		this.valor = valor;
+	public Orcamento() {
+		this.itens = new ArrayList<>();
 	}
 	
 	public double getValor() {
 		return valor;
 	}
 
+	public void adiciona(Item item) {
+		
+		this.valor += item.getValor();
+		this.itens.add(item);
+	}
+	
+	public List<Item> getItens() {
+		return Collections.unmodifiableList(itens);
+	}
 }
